ee8f6472b7559ee606036fa1e1bafbddd18edc5d
Me3enov <art.mezenow@yandex.ru>
2021-02-05 22:24:42 +0300

    project_6 iteration_2

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,4 @@
+//validation config
 const validationConfig = {
   formSelector: '.form',
   formFieldset: '.form__input-container',
@@ -8,6 +9,7 @@ const validationConfig = {
   errorClass: 'form__input-error_active'
 };
 
+//show validation error in input
 const showInputError = (formElement, inputElement, errorMessage, config) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.add(config.inputErrorClass);
@@ -15,6 +17,7 @@ const showInputError = (formElement, inputElement, errorMessage, config) => {
   errorElement.classList.add(config.errorClass);
 };
 
+//hide validation error in input
 const hideInputError = (formElement, inputElement, config) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.remove(config.inputErrorClass);
@@ -22,18 +25,26 @@ const hideInputError = (formElement, inputElement, config) => {
   errorElement.textContent = '';
 };
 
+//check input
 const checkInputValidity = (formElement, inputElement, config) => {
+  //if not valid
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage, config);
+    //if valid
   } else {
     hideInputError(formElement, inputElement, config);
   }
 };
 
+//set event listeners in inputs
 const setEventListeners = (formElement, config) => {
+  //array all inputs
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  //submit button
   const buttonElement = formElement.querySelector(config.submitButtonSelector);
+  //toggle submit button state
   toggleButtonState (inputList, buttonElement);
+  //cycle for array inputs - add event listeners
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement, config);
@@ -42,9 +53,13 @@ const setEventListeners = (formElement, config) => {
   });
 };
 
+//enable validation (object)
 const enableValidation = (config) => {
+  //array all forms
   const formList = Array.from(document.querySelectorAll(config.formSelector));
+  //cycle for array forms
   formList.forEach((formElement) => {
+    //array all fieldset objects
     const fieldsetList = Array.from(formElement.querySelectorAll(config.formFieldset));
     fieldsetList.forEach((item) => {
       setEventListeners(item, config);})
@@ -52,25 +67,32 @@ const enableValidation = (config) => {
   );
 };
 
+//if invalid input return invalid form
 function hasInvalidInput (inputList) {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   })
 }
 
+//toggle submit button state
 function toggleButtonState (inputList, buttonElement) {
+  //if invalid - add inactive class & disabled state
   if(hasInvalidInput(inputList)) {
     buttonElement.classList.add(validationConfig.inactiveButtonClass);
     buttonElement.disabled = true;
   }
+  //if valid - remove inactive class & disabled state
   else {
     buttonElement.classList.remove(validationConfig.inactiveButtonClass);
     buttonElement.disabled = false;
   }
 }
 
+//clear validation errors
 function clearValidation (popup, config) {
+  //array all input
   const currentInput = popup.querySelectorAll(config.inputSelector);
+  //cycle for array inputs - clear validation errors in input
   currentInput.forEach((item) => {
     item.classList.remove(config.inputErrorClass);
     const currentElement = popup.querySelector(`#${item.id}-error`);
